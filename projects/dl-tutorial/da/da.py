36171fcc7418faeaac606f08a66ada3fb15d8426
Jakub Sygnowski <sygnowski@gmail.com>
2016-09-11 20:58:18 -0700

    working autoencoder

--- projects/dl-tutorial/da/da.py
+++ projects/dl-tutorial/da/da.py
@@ -3,8 +3,9 @@ import math
 import reading
 import random
 
+BATCH_SIZE = 20
 
-batch = reading.get_batch()  # need to create batch before starting queue runners
+batch = reading.get_batch(BATCH_SIZE)
 
 n_hidden = 500
 n_visible = 28 * 28
@@ -18,7 +19,7 @@ B = tf.Variable(tf.zeros((n_hidden,), dtype=tf.float32), name="biases")
 B_2 = tf.Variable(tf.zeros((n_visible,), dtype=tf.float32), name="biases_generator")
 W_2 = tf.transpose(W, name="weights_generator")
 
-sess = tf.InteractiveSession()
+sess = tf.Session()
 
 
 def get_encoded(inp):
@@ -26,36 +27,41 @@ def get_encoded(inp):
 
 
 def get_decoded(hidden):
-    return tf.sigmoid(tf.matmul(hidden, W_2) + B_2)
+    return tf.matmul(hidden, W_2) + B_2
 
 
-def get_train_op(inp):
+def get_train_op(inp, noise=False):
     # TODO: add noise to inp
     z = get_encoded(inp)
     x_hat = get_decoded(z)
-    loss = tf.reduce_mean(
-            tf.nn.sigmoid_cross_entropy_with_logits(inp, x_hat, name="loss"))
-    
-    learning_rate = 0.3
+    loss = tf.reduce_mean(tf.reduce_sum(
+        tf.nn.sigmoid_cross_entropy_with_logits(x_hat, inp, name="loss"), 1), 0)
+
+    learning_rate = 0.1  # originally: 0.1
+    global_step = tf.get_variable("global_step", dtype=tf.int32,
+                                  initializer=0, trainable=False)
     optimizer = tf.train.GradientDescentOptimizer(learning_rate)
-    loss_summary_op = tf.scalar_summary("loss", loss)
-    x_reshaped = tf.reshape(inp, [128, 28, 28, 1])
-    x_hat_reshaped = tf.reshape(x_hat, [128, 28, 28, 1])
-    inp_summary_op = tf.image_summary("input", x_reshaped)
-    im_summary_op = tf.image_summary("reconstructed", x_hat_reshaped)
+    tf.scalar_summary("loss", loss)
+    x_reshaped = tf.reshape(inp, [BATCH_SIZE, 28, 28, 1])
+    x_hat_reshaped = tf.reshape(tf.sigmoid(x_hat), [BATCH_SIZE, 28, 28, 1])
+    tf.image_summary("input", x_reshaped)
+    tf.image_summary("reconstructed", x_hat_reshaped)
+    tf.histogram_summary("weights", W)
 
-    return optimizer.minimize(loss), loss
+    return optimizer.minimize(loss, global_step=global_step), loss, global_step
 
-train_op, loss_op = get_train_op(batch)
+train_op, loss_op, step_op = get_train_op(batch)
 merged_op = tf.merge_all_summaries()
-writer = tf.train.SummaryWriter("/tmp/mnist_logs", sess.graph)
-reading.initialize_session(sess)
+writer = tf.train.SummaryWriter("/tmp/mnist_logs/4", sess.graph, max_queue=1000)
+filling_thread = reading.initialize_session(sess)
 
-while True:
-    _, l, summary = sess.run([train_op, loss_op, merged_op])
-    if random.randint(0, 10) == 3:
-        writer.add_summary(summary)
-        print("loss: ", l)
-        writer.flush()
+try:
+    while True:
+        _, l, summary, step = sess.run([train_op, loss_op, merged_op, step_op])
+        writer.add_summary(summary, global_step=step)
+        if random.randint(0, 1000) == 3:
+            print("loss: ", l)
 
+except tf.errors.OutOfRangeError:
+    print "finished"
 # TODO: some code to join the threads to be able to leave ipython
--- projects/dl-tutorial/da/reading.py
+++ projects/dl-tutorial/da/reading.py
@@ -14,21 +14,20 @@ data_file = gzip.open("mnist.pkl.gz")
 data_file.close()
 
 fifo_queue = tf.FIFOQueue(capacity=10, dtypes=[tf.float32], shapes=[784])
-subs = train_np[:10000, :]
-enqueue_op = fifo_queue.enqueue_many(subs)
+# subs = train_np[:10000, :]
+enqueue_op = fifo_queue.enqueue_many(train_np)
 
-
-def fill_fifo_queue(sess):
+def fill_fifo_queue(sess):  # TODO: substitute with QueueRunner somehow
     NUM_EPOCHS = 15
     for i in range(NUM_EPOCHS):
-        print "enqueueing"
+        print("epoch ", i)
         sess.run(enqueue_op)
     sess.run(fifo_queue.close())
 
 
-def get_batch():
+def get_batch(batch_size=20):
     example = fifo_queue.dequeue()
-    batch = tf.train.shuffle_batch([example], batch_size=128, capacity=12800, min_after_dequeue=500, seed=None, enqueue_many=False)
+    batch = tf.train.shuffle_batch([example], batch_size=batch_size, capacity=12800, min_after_dequeue=500, seed=None, enqueue_many=False)
     return batch
 
 
@@ -38,4 +37,4 @@ def initialize_session(sess):
     enqueue_thread.start()
     sess.run(tf.initialize_all_variables())
     sess.run(tf.initialize_local_variables())
-
+    return enqueue_thread
